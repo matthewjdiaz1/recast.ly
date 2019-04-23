1e636ec1a0d76d0428997c2d60998f9e8494ba81
matthewjdiaz1 <matthewjdiaz1@gmail.com>
2019-04-22 20:29:14 -0700

    starting fresh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+// // In the ES6 spec, files are "modules" and do not share a top-level scope
+// // `var` declarations will only exist globally where explicitly defined
+// export default App;
+
+// The top level container for the entire application. This is the component that will be rendered to the DOM
+import exampleVideoData from './data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +19,8 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+        <VideoList videoName={exampleVideoData}/>
       </div>
     </div>
   </div>
